a306b484bc76a1b14d81a0c734388d4a7b1e1212
mgraciano <mgraciano@mgraciano-laptop>
2012-07-25 09:52:59 -0300

    Sample without use of @QueryName

--- typedquery-demo/src/main/java/j1/model/Index.java
+++ typedquery-demo/src/main/java/j1/model/Index.java
@@ -2,6 +2,7 @@ package j1.model;
 
 import j1.ci.QueryName;
 import j1.databeans.Customer;
+import j1.databeans.Product;
 import java.util.List;
 import javax.enterprise.inject.Model;
 import javax.inject.Inject;
@@ -11,10 +12,16 @@ import javax.persistence.TypedQuery;
 public class Index {
 
     @Inject
+    TypedQuery<Product> findAll;
+    @Inject
     @QueryName("Customer.findAll")
     TypedQuery<Customer> customers;
 
     public List<Customer> customers() {
         return customers.getResultList();
+    }
+
+    public List<Product> products() {
+        return findAll.getResultList();
     }
 }
